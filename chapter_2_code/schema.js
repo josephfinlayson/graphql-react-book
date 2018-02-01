27d4411c91b93c973175a502d78ebd72afa1478b
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-02-01 17:44:04 +0300

    chap2-first introduction to express

---
+++ chapter_2_code/schema.js
@@ -0,0 +1,15 @@
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
+});
+
+export schema;
